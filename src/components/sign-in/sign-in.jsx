e1ef0ea2677b57e7159ac5c80bf9c3e78767590c
anubhavvs <anubhavvsaha2001@gmail.com>
2020-07-04 16:50:53 +0530

    fixed labels

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -48,7 +48,7 @@ class SignIn extends React.Component {
                         type='email'
                         handleChange={this.handleChange}
                         value={this.state.email}
-                        label='email'
+                        label='Email'
                         required
                     />
                     <FormInput
@@ -56,12 +56,12 @@ class SignIn extends React.Component {
                         type='password'
                         value={this.state.password}
                         handleChange={this.handleChange}
-                        label='password'
+                        label='Password'
                         required
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign In </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
                     </div>
                 </form>
             </div>
