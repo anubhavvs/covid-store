5794f9820e354d010bf68c8d64cceb4772e7379e
anubhavvs <anubhavvsaha2001@gmail.com>
2020-07-04 16:06:03 +0530

    added sign-up component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,15 +12,41 @@ const config = {
     messagingSenderId: "44493867360",
     appId: "1:44493867360:web:a1974e59e4511184dd7605",
     measurementId: "G-29VFQ0JT3F"
-  }
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+    
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+    const snapShot = await userRef.get();
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
 
-  export default firebase;
+    return userRef;
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
