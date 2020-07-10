5fc49f70ab2e571e073675d1e6404313c406f966
anubhavvs <anubhavvsaha2001@gmail.com>
2020-07-10 19:51:11 +0530

    navigation fix

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item';
 
 import './collection-preview.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title' onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}>{title.toUpperCase()}</h1>
         <div className='preview'>
         {items
             .filter((item, idx) => idx < 4)
@@ -17,4 +18,4 @@ const CollectionPreview = ({ title, items }) => (
     </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
