331798f8fd253bf66f6255454ba94c4c9e7382d1
anubhavvs <anubhavvsaha2001@gmail.com>
2020-07-16 13:54:50 +0530

    fixed styling and added mobile view support

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -39,7 +39,7 @@ class SignIn extends React.Component {
     render() {
         return (
             <div className='sign-in'>
-                <h1>I already have an account</h1>
+                <h1 className='title'>I already have an account</h1>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
